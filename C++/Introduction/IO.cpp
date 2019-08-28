ea54ae5c4565036982fec7190b3492173c2dae06
jay619 <shahjaynish@gmail.com>
2019-08-27 22:15:16 -0400

    Input and Output

---
+++ C++/Introduction/IO.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int x;
+    int y;
+    int z;
+
+    cin >> x >> y >> z; 
+    cout << (x+y+z) << endl;
+    return 0;
+}
